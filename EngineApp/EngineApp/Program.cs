93962cdbcb1f34e4175bc447a9c00d6a034ce927
Lasse Andreasen <unterhugo@gmail.com>
2019-01-24 16:04:22 +0100

    start engine

---
+++ EngineApp/EngineApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EngineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello - I am engine app!");
+        }
+    }
+}
