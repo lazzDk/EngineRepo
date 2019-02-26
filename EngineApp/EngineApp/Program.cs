8ffa63bb68222326badde3f60b727baafcdb7696
Lasse Andreasen <unterhugo@gmail.com>
2019-02-26 14:43:48 +0100

    hello there

--- EngineApp/EngineApp/Program.cs
+++ EngineApp/EngineApp/Program.cs
@@ -9,6 +9,7 @@ namespace EngineApp
             Console.WriteLine("Hello - I am engine app!");
             Console.WriteLine("Hello - I am using V6!");
             Console.WriteLine("The best gum");
+            var hello = "there";
         }
     }
 }
