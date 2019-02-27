a222ebb42aa482c266a959daf56187802926fbf0
Lasse Andreasen <unterhugo@gmail.com>
2019-02-27 10:42:04 +0100

    this is development

--- EngineApp/EngineApp/Program.cs
+++ EngineApp/EngineApp/Program.cs
@@ -7,6 +7,7 @@ namespace EngineApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello - I am engine app!");
+            Console.WriteLine("This is development");
         }
     }
 }
